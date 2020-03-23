60aabaa746a10f084269d2e3896a51677f15efe8
IslandMoney <olgermile@yahoo.com>
2020-03-23 17:42:27 +0100

    sg config change

--- src/mail/account.js
+++ src/mail/account.js
@@ -5,7 +5,7 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'olger@olgermile.com',
+        from: 'mail@olgermile.com',
         subject: 'Task App Welcome Email Subject',
         text: `Hello there ${name}!`
     })
@@ -14,7 +14,7 @@ const sendWelcomeEmail = (email, name) => {
 const sendCancellationEmail = (email, name) => {
     sgMail.send({
         to: email,
-        from: 'olger@olgermile.com',
+        from: 'mail@olgermile.com',
         subject: 'Task App Deleting Account Subject',
         text: `Hello ${name}, we are sad to see you go! Let us know if there is anything to have kept you on board by replying to this email!`
     })
